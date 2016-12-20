9dc3160bc60671a4d705d0c633202f3a4c19b838
Anna Mefford <annamefford@gmail.com>
2016-12-20 15:00:28 -0700

    added article pages with links

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    resources :articles
+    root 'welcome#index'
+  end
+
 end
